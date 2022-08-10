0ffeac0df36af3a97222c56a520b7c2eb3a7875d
InnaSolod <solodinna6@gmail.com>
2022-08-10 16:20:47 +0300

    save

--- js-functions/08-arguments.js
+++ js-functions/08-arguments.js
@@ -8,12 +8,26 @@
 //   console.log(args);
 // };
 
-const fn = function (...args) {
-  console.log(args);
-};
+// const fn = function (...args) {
+//   console.log(args);
+// };
 
-fn(1, 2, 3);
-fn(1, 2, 3, 4, 5);
-fn(1, 2, 3, 4, 5, 6, 7);
+// fn(1, 2, 3);
+// fn(1, 2, 3, 4, 5);
+// fn(1, 2, 3, 4, 5, 6, 7);
 
 // Напиши функцию add для сложения произвольного количества аргументов (чисел)
+
+const add = function (...args) {
+  console.log(args);
+  let total = 0;
+
+  for (const arg of args) {
+    total += arg;
+  }
+
+  return total;
+};
+
+console.log(add(1, 2, 3));
+console.log(add(1, 2, 4, 5, 6));
